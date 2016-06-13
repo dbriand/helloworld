7ab478bb5dd609bafeb7a20588b2a362f18ae835
David Briand <david.briand@sympatico.ca>
2016-06-13 10:56:24 -0400

    commit on June 13, 2016

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -5,5 +5,6 @@ public class JavaHelloWorld
 		System.out.println("Java Hello World");
 		System.out.println("Hello Again");
 		System.out.println("Hello Again Again");
+		System.out.println("Hello Again Jule 13, 2016");
 	}
 }
